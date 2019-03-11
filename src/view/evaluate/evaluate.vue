4c4518b01d1426aaaa6e074dbfd2c67b02801a09
ICARUSxyz <4620600447@qq.com>
2019-03-11 13:57:20 +0800

    修改evaluate&find样式

--- src/view/evaluate/evaluate.vue
+++ src/view/evaluate/evaluate.vue
@@ -5,14 +5,12 @@
       <el-form label-width="100px">
         <div style="height:15px"></div>
         <h2 style="text-align: center">发布流浪动物信息</h2>
-        <div style="height:15px"></div>
-        <div class="div-a">
-          <el-form-item>
+        <div style="height:15px">
+          <el-form-item >
             <span slot="label">名&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;称</span>
-            <el-input type="text" placeholder style="width:217px"></el-input>
+            <el-input type="text" placeholde style="width:217px"></el-input>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item required>
             <span slot="label">类&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;别</span>
             <el-select placeholder="请选择">
@@ -24,8 +22,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item required>
             <!--先放一放，问题很大-->
             <span slot="label">性&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;别</span>
@@ -38,14 +35,12 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item required>
             <span slot="label" >年&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;龄</span>
             <el-input type="text" placeholder="请输入年龄（单位为月份）" style="width:217px"></el-input>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="主要血统" required>
             <el-select placeholder="请选择">
               <el-option
@@ -56,8 +51,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="次要血统">
             <el-select placeholder="请选择">
               <el-option
@@ -68,8 +62,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="主要毛色" required>
             <el-select placeholder="请选择">
               <el-option
@@ -80,8 +73,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="次要毛色">
             <el-select placeholder="请选择">
               <el-option
@@ -92,8 +84,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="再次毛色">
             <el-select placeholder="请选择">
               <el-option
@@ -104,8 +95,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="成熟体型" required>
             <el-select placeholder="请选择">
               <el-option
@@ -116,8 +106,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item required>
             <span slot="label" required>毛&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;长</span>
             <el-select placeholder="请选择">
@@ -129,8 +118,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="所在州域" required>
             <el-select placeholder="请选择">
               <el-option
@@ -154,8 +142,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="有无疫苗" required>
             <el-select placeholder="请选择">
               <el-option
@@ -166,8 +153,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="是否绝育" required>
             <el-select placeholder="请选择"> 
               <el-option
@@ -178,8 +164,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="健康状况" required>
             <el-select placeholder="请选择">
               <el-option
@@ -191,10 +176,8 @@
             </el-select>
           </el-form-item>
         </div>
-        <el-form-item label="动物描述">
-          <el-input type="textarea" rows="3" style="width:590px"></el-input>
-        </el-form-item>
-        <div class="div-a">
+        
+ 
           <el-form-item label="动物图片">
             <el-upload
               class="upload-demo"
@@ -213,8 +196,7 @@
               <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
             </el-upload>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="动物视频">
             <el-upload
               class="upload-demo"
@@ -233,10 +215,15 @@
               <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
             </el-upload>
           </el-form-item>
-        </div>
+        
         <!--<el-form-item label="信息确认">-->
         <!--<el-button type="primary" @click="get_url">确认上传图片</el-button>-->
         <!--</el-form-item>-->
+
+        <el-form-item label="动物描述">
+          <el-input type="textarea" rows="3" style="width:590px"></el-input>
+        </el-form-item>
+
         <el-form-item style="text-align: center" label-width="-50px">
           <el-button type="primary" @click="handle_register">提交</el-button>
         </el-form-item>
@@ -270,8 +257,9 @@ export default {
   border-radius: 2%;
   background: rgba(255, 255, 255, 0.9);
 }
-.div-a {
+.el-form-item {
   float: left;
   width: 49%;
 }
+
 </style>
--- src/view/find/find.vue
+++ src/view/find/find.vue
@@ -5,13 +5,12 @@
       <el-form label-width="100px">
         <div style="height:15px"></div>
         <h2 style="text-align: center">添加你的喜欢参数</h2>
-        <div style="height:15px"></div>
-        <div class="div-a">
+        <div style="height:15px">
+        </div>
           <el-form-item label="领养费用">
             <el-input type="text" placeholder="货币单位MYR" style="width:217px"></el-input>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item>
             <span slot="label">类&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;别</span>
             <el-select placeholder="请选择">
@@ -23,8 +22,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item>
             <!--先放一放，问题很大-->
             <span slot="label">性&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;别</span>
@@ -37,14 +35,12 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item>
             <span slot="label">年&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;龄</span>
             <el-input type="text" placeholder="请输入年龄（单位为月份）" style="width:217px"></el-input>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="主要血统">
             <el-select placeholder="请选择">
               <el-option
@@ -55,8 +51,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="次要血统">
             <el-select placeholder="请选择">
               <el-option
@@ -67,8 +62,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="主要毛色">
             <el-select placeholder="请选择">
               <el-option
@@ -79,8 +73,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="次要毛色">
             <el-select placeholder="请选择">
               <el-option
@@ -91,8 +84,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="再次毛色">
             <el-select placeholder="请选择">
               <el-option
@@ -103,8 +95,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="成熟体型">
             <el-select placeholder="请选择">
               <el-option
@@ -115,8 +106,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item>
             <span slot="label">毛&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;长</span>
             <el-select placeholder="请选择">
@@ -128,8 +118,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="所在州域">
             <el-select placeholder="请选择">
               <el-option
@@ -140,8 +129,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="是否除虫">
             <el-select placeholder="请选择">
               <el-option
@@ -152,8 +140,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="有无疫苗">
             <el-select placeholder="请选择">
               <el-option
@@ -164,8 +151,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="是否绝育">
             <el-select placeholder="请选择">
               <el-option
@@ -176,8 +162,7 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <div class="div-a">
+        
           <el-form-item label="健康状况">
             <el-select placeholder="请选择">
               <el-option
@@ -188,9 +173,8 @@
               ></el-option>
             </el-select>
           </el-form-item>
-        </div>
-        <el-form-item style="text-align: center" label-width="-50px">
-          <el-button type="primary" @click="handle_register">提交</el-button>
+             <el-form-item style="text-align: center" >
+          <el-button type="primary" style="float: right;margin-right:-50px" @click="handle_register">提交</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -217,7 +201,7 @@ export default {
   border-radius: 2%;
   background: rgba(255, 255, 255, 0.9);
 }
-.div-a {
+.el-form-item {
   float: left;
   width: 49%;
 }
