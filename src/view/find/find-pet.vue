a632df47f0fe5b46692bf3b10bad2ed9d24e58eb
FFT <liufft98@gmail.com>
2019-03-11 10:15:44 +0800

    add find-pet

---
+++ src/view/find/find-pet.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="o in 4" :key="o" class="text item">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>卡片名称</span>
+          <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+        </div>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "find-pet"
+    }
+</script>
+
+<style scoped>
+
+</style>
