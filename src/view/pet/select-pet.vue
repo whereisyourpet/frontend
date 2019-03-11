532824f5bb0727eadfb54bbf76ab814d65e4fc4b
FFT <liufft98@gmail.com>
2019-03-11 20:30:44 +0800

    add select-pet

--- src/view/pet/select-pet.vue
+++ src/view/pet/select-pet.vue
@@ -4,9 +4,9 @@
       <el-col :span="8">
         <div class="select-block">
           <span slot="label">请选择种类</span>
-          <el-select v-model="value1" filterable placeholder="请选择">
+          <el-select v-model="type" filterable placeholder="请选择">
             <el-option
-              v-for="item in options"
+              v-for="item in type_option"
               :key="item.value"
               :label="item.label"
               :value="item.value">
@@ -17,9 +17,9 @@
       <el-col :span="8">
         <div class="select-block">
           <span slot="label">请选择数量&nbsp&nbsp&nbsp</span>
-          <el-select v-model="value8" filterable placeholder="请选择">
+          <el-select v-model="quantities" filterable placeholder="请选择">
             <el-option
-              v-for="item in options"
+              v-for="item in quantities_option"
               :key="item.value"
               :label="item.label"
               :value="item.value">
@@ -30,9 +30,9 @@
       <el-col :span="8">
         <div class="select-block">
           <span slot="label">请选择颜色</span>
-          <el-select v-model="value8" filterable placeholder="请选择">
+          <el-select v-model="color" filterable placeholder="请选择">
             <el-option
-              v-for="item in options"
+              v-for="item in color_option"
               :key="item.value"
               :label="item.label"
               :value="item.value">
@@ -43,9 +43,9 @@
       <el-col :span="8">
         <div class="select-block">
           <span slot="label">请选择性别</span>
-          <el-select v-model="value8" filterable placeholder="请选择">
+          <el-select v-model="gender" filterable placeholder="请选择">
             <el-option
-              v-for="item in options"
+              v-for="item in gender_option"
               :key="item.value"
               :label="item.label"
               :value="item.value">
@@ -56,9 +56,9 @@
       <el-col :span="8">
         <div class="select-block">
           <span slot="label">请选择所在州</span>
-          <el-select v-model="value8" filterable placeholder="请选择">
+          <el-select v-model="state" filterable placeholder="请选择">
             <el-option
-              v-for="item in options"
+              v-for="item in state_option"
               :key="item.value"
               :label="item.label"
               :value="item.value">
@@ -85,7 +85,114 @@
 
 <script>
     export default {
-        name: "select-pet"
+        name: "select-pet",
+        data(){
+          return{
+            type:'',
+            quantities:'',
+            color:'',
+            gender:'',
+            state:'',
+            type_option:[
+              {
+                value:'1',
+                label:'苟'
+              },
+              {
+                value:'2',
+                label:'毛'
+              }
+            ],
+            quantities_option:[
+            //  没人回复我，我就先空下了
+            ],
+            color_option:[
+              { value: "1", label: "Black" },
+              { value: "2", label: "Brown" },
+              { value: "3", label: "Golden" },
+              { value: "4", label: "Yellow" },
+              { value: "5", label: "Cream" },
+              { value: "6", label: "Gray" },
+              { value: "7", label: "White" }
+            ],
+            gender_option:[
+              {
+                value: "1",
+                label: "公"
+              },
+              {
+                value: "2",
+                label: "母"
+              },
+              {
+                value: "3",
+                label: "混合"
+              }
+            ],
+            state_option:[
+              {
+                value: "1",
+                label: "Johor"
+              },
+              {
+                value: "2",
+                label: "Kedah"
+              },
+              {
+                value: "3",
+                label: "Kelantan"
+              },
+              {
+                value: "4",
+                label: "Kuala Lumpur"
+              },
+              {
+                value: "5",
+                label: "Labuan"
+              },
+              {
+                value: "6",
+                label: "Melaka"
+              },
+              {
+                value: "7",
+                label: "Negeri Sembilan"
+              },
+              {
+                value: "8",
+                label: "Pahang"
+              },
+              {
+                value: "9",
+                label: "Perak"
+              },
+              {
+                value: "10",
+                label: "Perlis"
+              },
+              {
+                value: "11",
+                label: "Pulau Pinang"
+              },
+              {
+                value: "12",
+                label: "Sabah"
+              },
+              {
+                value: "13",
+                label: "Sarawak"
+              },
+              {
+                value: "14",
+                label: "Selangor"
+              },
+              {
+                value: "15",
+                label: "Terengganu"
+              }
+            ]
+        }
+      }
     }
 </script>
 
@@ -95,7 +202,7 @@
   padding-top: 3%;
 }
 .confirm-button{
-  padding-left: 8%;
+  padding-left: 25%;
   padding-top: 3%;
 }
 </style>
