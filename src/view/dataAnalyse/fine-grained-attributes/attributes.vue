9014e732a883ae5c98b4a74a6d336bc99c2bbcb1
FFT <liufft98@gmail.com>
2019-03-12 10:36:25 +0800

    fine-grained attri

--- src/view/dataAnalyse/fine-grained-attributes/attributes.vue
+++ src/view/dataAnalyse/fine-grained-attributes/attributes.vue
@@ -6,6 +6,7 @@
       <el-button round @click="$router.push('/')">毛长</el-button>
       <el-button round @click="$router.push('/')">驱虫</el-button>
       <el-button round @click="$router.push('/')">收养费用</el-button>
+      <el-button round @click="$router.push('/')">地域</el-button>
     </div>
 </template>
 
