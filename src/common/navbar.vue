809e9b62ba9ca99d7a53daf89c942eb73c5085cb
fu-huo <15209201587@163.com>
2019-03-12 14:35:46 +0800

    不说话

--- src/common/navbar.vue
+++ src/common/navbar.vue
@@ -71,6 +71,10 @@ export default {
             me.$axios.defaults.withCredentials = true;
             me.$axios(setting).then(function(response) {
               me.nickname = response.data.nickname;
+              if (response.data.willing == "") {
+                alert("请完善个人信息");
+                me.$router.push("basic");
+              }
             });
           }
         }
