be30bb756d6e04a597205b685a7a2d7d84e8472a
FFT <liufft98@gmail.com>
2019-03-12 09:40:05 +0800

    published-pet list css change

--- src/view/user/content/published-pet.vue
+++ src/view/user/content/published-pet.vue
@@ -3,14 +3,14 @@
     <div class="" >
       <div class="" v-for="publishedPet in publishedPets" :key="publishedPet.id">
         <div @click="$router.push('/Pet/'+publishedPet.id)">
-          <el-card class="organization-card">
+          <el-card class="published-pet-card">
             <el-container>
-              <el-main class="organization-info-right">
-                <div class="organization-title word-style" style="font-size: xx-large; ">
-                  <a @click="$router.push('/Pet/'+publishedPet.id)" style="text-decoration: none">组织名称：{{ publishedPet.name }}</a>
+              <el-main class="published-pet-info-right">
+                <div class="published-pet-title word-style" style="font-size: xx-large; ">
+                  <a @click="$router.push('/Pet/'+publishedPet.id)" style="text-decoration: none">宠物名称：{{ publishedPet.name }}</a>
                 </div>
-                <div class="organization-time word-style" style="font-size: x-large; ">
-                  负责人：{{publishedPet.name}}
+                <div class="published-pet-time word-style" style="font-size: x-large; ">
+                  发布人：{{publishedPet.name}}
                 </div>
               </el-main>
             </el-container>
@@ -29,5 +29,23 @@
 </script>
 
 <style scoped>
+  .published-pet-list{
+    padding-top: 3%;
+    width: 80%;
+    padding-left: 10%;
+    /*height: 200px;*/
 
+  }
+  .published-pet-card{
+    height: 200px;
+  }
+  .word-style{
+    padding-left: 3%;
+    padding-bottom: 3%;
+  }
+  .published-pet-button{
+    float: right;
+    /*padding-top: 2%;*/
+    padding-right: 10%;
+  }
 </style>
