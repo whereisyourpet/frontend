3d7fb9a37cd80ed21b77ae1756f3e460e6870723
FFT <liufft98@gmail.com>
2019-03-14 10:39:55 +0800

    evaluate-result

--- src/view/evaluate/evaluate_result.vue
+++ src/view/evaluate/evaluate_result.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="background">
     <el-card class="center-block">
-      <h2>收养指数为{{this.Global.predict_adoption_star}}</h2>
+      <!--<h2 v-model="adoption_star">收养指数为{{this.Global.predict_adoption_star}}</h2>-->
+      <h2 v-model="adoption_star">收养指数为{{(adoption_star + 0.2) | numFilter}}</h2>
       <hr>
-      <h2>受欢迎指数为{{this.Global.predict_popularity_star}}</h2>
+      <h2 v-model="adoption_star">超越了{{((4-(adoption_star + 0.2)) / 4) * 100}} %的宠物</h2>
+      <hr>
+      <h2 v-model="popularity_star">受欢迎指数为{{popularity_star}}</h2>
+      <hr>
+      <h2 v-model="popularity_star">超越了{{((4-popularity_star) / 4) * 100}} %的宠物</h2>
     </el-card>
   </div>
 </template>
@@ -11,10 +16,32 @@
 export default {
   name: "evaluate_pet",
   data() {
-    return {};
+    return {
+      adoption_star :'',
+      popularity_star:''
+    };
+  },
+  filters: {
+
+    numFilter(value) {
+
+      // 截取当前数据到小数点后三位
+
+      let transformVal = Number(value).toFixed(3)
+
+      let realVal = transformVal.substring(0, transformVal.length - 1)
+
+      // num.toFixed(3)获取的是字符串
+
+      return Number(realVal)
+
+    }
+
   },
   mounted() {
     let me = this;
+    me.adoption_star = me.Global.predict_adoption_star
+    me.popularity_star = me.Global.predict_popularity_star
     console.log(me.Global.find_data);
   },
   //  接口没定我写个捷豹写,
@@ -35,7 +62,7 @@ export default {
 
 .center-block {
   position: absolute;
-  top: 40%;
+  top: 23%;
   left: 55%;
   overflow: hidden;
   text-align: center;
